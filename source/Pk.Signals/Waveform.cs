13069fe798534ac7cd1d3e0cb234fc417df981c3
Jules LaPrairie <julesl@pksound.ca>
2016-11-03 15:54:44 -0600

    Added more construction option to VoltageSignal. Added Peak and RMS properties

--- source/Pk.Signals/Waveform.cs
+++ source/Pk.Signals/Waveform.cs
@@ -6,20 +6,30 @@ namespace Pk.Signals
 {
   public sealed class Waveform : Enumeration<Waveform>
   {
-    public static readonly Waveform Sinusoid = new Waveform(0, nameof(Sinusoid), peak => peak / Math.Sqrt(2.0));
-    public static readonly Waveform Square = new Waveform(1, nameof(Sinusoid), peak => peak);
-    public static readonly Waveform Triangle = new Waveform(2, nameof(Sinusoid), peak => peak / Math.Sqrt(3.0));
-    public static readonly Waveform Sawtooth = new Waveform(3, nameof(Sinusoid), peak => peak / Math.Sqrt(3.0));
+    public static readonly Waveform Sinusoid = new Waveform(0, nameof(Sinusoid), peak => peak/Math.Sqrt(2.0),
+                                                            rms => rms*Math.Sqrt(2.0));
+
+    public static readonly Waveform Sawtooth = new Waveform(3, nameof(Sinusoid), peak => peak/Math.Sqrt(3.0),
+                                                            rms => rms*Math.Sqrt(3.0));
+
+    public static readonly Waveform Square = new Waveform(1, nameof(Sinusoid), peak => peak, rms => rms);
+
+    public static readonly Waveform Triangle = new Waveform(2, nameof(Sinusoid), peak => peak/Math.Sqrt(3.0),
+                                                            rms => rms*Math.Sqrt(3.0));
+
+    private readonly Func<ElectricPotential, ElectricPotential> calculatePeak;
     private readonly Func<ElectricPotential, ElectricPotential> calculateRms;
 
 
-    private Waveform(int value, string displayName, Func<ElectricPotential, ElectricPotential> calculateRms)
-        : base(value, displayName)
+    private Waveform(int value, string displayName, Func<ElectricPotential, ElectricPotential> calculateRms,
+                     Func<ElectricPotential, ElectricPotential> calculatePeak) : base(value, displayName)
     {
       this.calculateRms = calculateRms;
+      this.calculatePeak = calculatePeak;
     }
 
-    
+
+    public ElectricPotential CalculatePeak(ElectricPotential rms) { return this.calculatePeak(rms); }
     public ElectricPotential CalculateRms(ElectricPotential peak) { return this.calculateRms(peak); }
   }
 }
--- tests/Pk.Signals.Tests/WaveformTests.cs
+++ tests/Pk.Signals.Tests/WaveformTests.cs
@@ -13,18 +13,48 @@ namespace Pk.Signals.Tests
 
     public static readonly IEnumerable<object[]> RmsExpectations = new[]
                                                                    {
-                                                                       new object[] {Waveform.Sawtooth, 9.123456789, 5.267430233}, 
-                                                                       new object[] {Waveform.Sinusoid, 3.568985612375,2.523653928 }, 
-                                                                       new object[] {Waveform.Square, 1.000000025, 1.000000025}, 
-                                                                       new object[] {Waveform.Triangle, 78.468523458,45.303823141 }, 
+                                                                       new object[]
+                                                                       {
+                                                                           Waveform.Sawtooth,
+                                                                           9.123456789,
+                                                                           5.267430233
+                                                                       },
+                                                                       new object[]
+                                                                       {
+                                                                           Waveform.Sinusoid,
+                                                                           3.568985612375,
+                                                                           2.523653928
+                                                                       },
+                                                                       new object[]
+                                                                       {
+                                                                           Waveform.Square,
+                                                                           1.000000025,
+                                                                           1.000000025
+                                                                       },
+                                                                       new object[]
+                                                                       {
+                                                                           Waveform.Triangle,
+                                                                           78.468523458,
+                                                                           45.303823141
+                                                                       }
                                                                    };
+
 
     [Theory]
     [MemberData(nameof(RmsExpectations))]
-    public void ShouldCalculateRms(Waveform waveform, double peakVoltage, double expectedRmsVoltage)
+    public void ShouldCalculatePeak(Waveform waveform, double peakVoltage, double rmsVoltage)
+    {
+      var result = waveform.CalculatePeak(ElectricPotential.FromVolts(rmsVoltage));
+      result.Volts.ShouldBe(peakVoltage, RmsTolerance);
+    }
+
+
+    [Theory]
+    [MemberData(nameof(RmsExpectations))]
+    public void ShouldCalculateRms(Waveform waveform, double peakVoltage, double rmsVoltage)
     {
       var result = waveform.CalculateRms(ElectricPotential.FromVolts(peakVoltage));
-      result.Volts.ShouldBe(expectedRmsVoltage, RmsTolerance);
+      result.Volts.ShouldBe(rmsVoltage, RmsTolerance);
     }
   }
 }
